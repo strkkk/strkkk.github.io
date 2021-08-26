a6a17393473e298ce033ce22449f34e807049044
Andrei Paikin <andreypaykin@gmail.com>
2021-08-26 16:58:56 +0300

    Update compare_versions.sh

--- useful/compare_versions.sh
+++ useful/compare_versions.sh
@@ -3,7 +3,7 @@ POM_BUILD_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStd
 # cleanup trailing zero if present
 BUILD_VERSION=$(echo "$POM_BUILD_VERSION" | sed -e 's/\.0$//g')
 
-echo "PAS version from pom.xml: $POM_BUILD_VERSION, build version to compare: $BUILD_VERSION"
+echo "version from pom.xml: $POM_BUILD_VERSION, build version to compare: $BUILD_VERSION"
 
 # transform branch name like rc_int_21-3 -> 21.3
 GIT_VERSION=$(echo "$1" | sed -e 's/.*_//g' -e 's/-/\./g')
